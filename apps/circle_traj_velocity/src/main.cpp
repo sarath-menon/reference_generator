b0727cccca24e84846a709826ef80fc3025458a1
smenon <smenon@ethz.ch>
2021-10-26 22:17:35 +0200

    circle_velocity_traj working

--- apps/circle_traj_velocity/src/main.cpp
+++ apps/circle_traj_velocity/src/main.cpp
@@ -1,19 +1,23 @@
-#include "QuadPositionCmdPubSubTypes.h"
+#include "QuadVelocityCmdPubSubTypes.h"
 #include "default_participant.h"
 #include "default_publisher.h"
-#include "quadcopter_msgs/msgs/QuadPositionCmd.h"
+#include "quadcopter_msgs/msgs/QuadVelocityCmd.h"
 #include <chrono>
 #include <cstdlib>
 #include <future>
+#include <math.h>
 
-// Step size in degrees
-constexpr static float step_size = 0.1;
+// Step size in seconds
+constexpr static float dt = 0.1;
+constexpr static int dt_ms = dt * 1000;
 
-// center angle in degrees
+// angel in radians
 float theta = 0;
+// angular velocity in rad/s
+constexpr static float theta_dot = 1.5;
 
 // Radius of circle to be tracked (in meters)
-constexpr static float radius = 1.5;
+constexpr static float radius = 0.5;
 
 // Local center
 constexpr static float center_x = 0;
@@ -24,38 +28,44 @@ constexpr static float const_altitude = 1.5;
 
 int main() {
   // Quadcopter position msg
-  cpp_msg::QuadPositionCmd pos_msg{};
+  cpp_msg::QuadVelocityCmd velocity_msg{};
 
   // Create participant. Arguments-> Domain id, QOS name
   DefaultParticipant dp(0, "quad_reference_publisher");
 
   // Create publisher with msg type
-  DDSPublisher position_pub(idl_msg::QuadPositionCmdPubSubType(), "pos_cmd",
-                            dp.participant());
+  DDSPublisher velocity_pub(idl_msg::QuadVelocityCmdPubSubType(),
+                            "velocity_cmd", dp.participant());
 
   // Initialize publisher
-  position_pub.init();
+  velocity_pub.init();
 
-  // Go to circle start point
-  pos_msg.position.z = const_altitude;
-  pos_msg.position.x = 1;
-  position_pub.publish(pos_msg);
+  // // Delay for quad to catch up
+  // std::this_thread::sleep_for(std::chrono::seconds(5));
+  constexpr static int i_max = (2 * M_PI) / (theta_dot * dt);
+  constexpr static int circles_number = 4;
 
-  // Delay for quad to catch up
-  std::this_thread::sleep_for(std::chrono::seconds(5));
-
-  for (int i = 0; i < 3601; i++) {
+  // for (int i = 0; i < i_max * 2; i++) {
+  for (int i = 0; i < i_max * circles_number; i++) {
 
     // COmpute x,y coordinates
     // std::cout << "Timestep:" << i << '\n';
-    pos_msg.position.x = center_x + radius * cos(theta);
-    pos_msg.position.y = center_y + radius * sin(theta);
+    velocity_msg.velocity.x = -theta_dot * radius * sin(theta);
+    velocity_msg.velocity.y = theta_dot * radius * cos(theta);
+    velocity_msg.velocity.z = 0;
 
-    theta += step_size;
+    theta += (theta_dot * dt);
+    std::cout << "Angle:" << theta * 180 / M_PI << std::endl;
 
     // Delay for quad to catch up
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(dt_ms));
 
-    position_pub.publish(pos_msg);
+    velocity_pub.publish(velocity_msg);
   }
+
+  // Set velocity to zero at the end
+  velocity_msg.velocity.x = 0;
+  velocity_msg.velocity.y = 0;
+  velocity_msg.velocity.z = 0;
+  velocity_pub.publish(velocity_msg);
 }
