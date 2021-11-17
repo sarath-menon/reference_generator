8dd4ef200230e974b6e13772c0b997f540d5cd99
smenon <smenon@ethz.ch>
2021-11-18 00:24:54 +0100

    read text file line by line works

--- apps/grasping_first_version/src/set_parameters.cpp
+++ apps/grasping_first_version/src/set_parameters.cpp
@@ -1,7 +1,11 @@
 
 #include "grasper.h"
 #include "safety_checks.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 #include <yaml-cpp/yaml.h>
 
 void Grasper::set_parameters(const std::string path) {
@@ -23,15 +27,58 @@ void Grasper::set_parameters(const std::string path) {
   // Set target setpoints
 }
 
-void Grasper::set_setpoints(const std::string path) {
+// void Grasper::set_setpoints(const std::string path) {
 
+//   // Safety check, see if file exists
+//   safety_checks::yaml_file_check(path);
+
+//   // Load yaml file containing gains
+//   YAML::Node setpoint_yaml = YAML::LoadFile(path);
+
+//   desired_pos_.x = setpoint_yaml["x_desired"].as<float>(); // [constant]
+//   desired_pos_.y = setpoint_yaml["y_desired"].as<float>(); // [constant]
+//   desired_pos_.z = setpoint_yaml["z_desired"].as<float>(); // [constant]
+// }
+
+void Grasper::set_setpoints(const std::string path, const int index) {
   // Safety check, see if file exists
   safety_checks::yaml_file_check(path);
 
-  // Load yaml file containing gains
-  YAML::Node setpoint_yaml = YAML::LoadFile(path);
+  // create ofstream to read
+  std::ifstream csv_file(path);
 
-  desired_pos_.x = setpoint_yaml["x_desired"].as<float>(); // [constant]
-  desired_pos_.y = setpoint_yaml["y_desired"].as<float>(); // [constant]
-  desired_pos_.z = setpoint_yaml["z_desired"].as<float>(); // [constant]
+  // check if the file is open
+  if (!csv_file.is_open())
+    throw std::runtime_error("Could not open file");
+
+  // get line from file
+  std::vector<float> x_ref;
+  std::vector<float> y_ref;
+  std::vector<float> z_ref;
+  std::vector<float> time;
+  std::vector<float> data;
+  std::string line;
+
+  while (getline(csv_file,line)) {
+    std::stringstream s_stream(line);
+    while (s_stream.good()) {
+      std::string substr;
+      getline(s_stream, substr, ','); // get first string delimited by comma
+      std::cout << "Substr:" << substr<< std::endl;
+      data.push_back(std::stof(substr));
+    }
+  }
+
+  std::cout << "Size:" << data.size()<< std::endl;
+
+  for (int i = 0; i < data.size(); i+=4) {
+    std::cout << i << ": (" << data.at(i) << "," << data.at(i + 1) << ","
+              << data.at(i + 2) << ") time:" << data.at(i + 3) << std::endl;
+
+              
+  }
+
+
+
+  csv_file.close();
 }
