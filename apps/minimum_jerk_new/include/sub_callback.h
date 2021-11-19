03fb3f055ea4eea154aabd2e8c834dc3da3367d0
smenon <smenon@ethz.ch>
2021-11-19 19:12:09 +0100

    shifted grasping, muller libs to lib folder

--- apps/minimum_jerk_new/include/sub_callback.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "MocapPubSubTypes.h"
-#include "sensor_msgs/msgs/Mocap.h"
-
-// Subscriber data that needs to be accessed in main
-namespace sub {
-cpp_msg::Mocap mocap_msg{};
-} // namespace sub
