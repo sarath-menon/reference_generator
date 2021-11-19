cb065d8fc77da75c1b693b6c9323e3e24c0a157a
smenon <smenon@ethz.ch>
2021-11-19 23:03:27 +0100

    added muller lib to minimum jerk

--- apps/grasp_object/include/paths.h
+++ apps/grasp_object/include/paths.h
@@ -1,12 +1,16 @@
 #pragma once
-
 #include "paths.h"
+#include <filesystem>
 #include <string>
 
 namespace paths {
-const std::string folder_path = "apps/grasp_object/parameters/";
 
-const std::string waypoints_path = folder_path + "waypoints.csv";
+std::filesystem::path folder_path = std::filesystem::current_path();
 
-const std::string parameters_path = folder_path + "parameters.yaml";
+const std::string abs_path =
+    folder_path.u8string() + "/apps/grasp_object/parameters/";
+
+const std::string waypoints_path = abs_path + "waypoints.csv";
+
+const std::string parameters_path = abs_path + "parameters.yaml";
 } // namespace paths
