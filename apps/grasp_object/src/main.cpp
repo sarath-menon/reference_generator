2a0c195a74d42302925b87890e663234c48d7b8b
smenon <smenon@ethz.ch>
2021-11-20 19:26:29 +0100

    waypoints for square

--- apps/grasp_object/src/main.cpp
+++ apps/grasp_object/src/main.cpp
@@ -18,17 +18,11 @@ int main() {
   grasper.load_waypoints(paths::waypoints_path);
   bool result{};
 
-  // Step 1: go over object
-  // argument: index of waypoint in text file
-  result = grasper.go_over_object("srl_box", 1.5, px4_ctrl);
-
-  // Step 2: grasp object
-  // argument: object name, max allowed time
-  result = grasper.go_over_object("srl_box", 1.5, px4_ctrl);
-  // std::this_thread::sleep_for(std::chrono::seconds(2));
-
-  // step 3: drop off object
-  result = grasper.go_to_waypoint(5, mueller_ctrl);
+  result = grasper.go_to_waypoint(0, px4_ctrl);
+  result = grasper.go_to_waypoint(1, px4_ctrl);
+  result = grasper.go_to_waypoint(2, px4_ctrl);
+  result = grasper.go_to_waypoint(3, px4_ctrl);
+  result = grasper.go_to_waypoint(4, px4_ctrl);
 
   // std::cout << "Target status:" << status << std::endl;
 }
