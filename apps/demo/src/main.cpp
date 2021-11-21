bbe66cc32e69915f6cad8435caa146e7f7e56c60
smenon <smenon@ethz.ch>
2021-11-21 20:53:29 +0100

    added comments

--- apps/demo/src/main.cpp
+++ apps/demo/src/main.cpp
@@ -6,10 +6,13 @@
 
 int main() {
 
+  // register domain participant to flight manager
   FlightManager::register_dds(DDS.participant());
 
+  // create a flight manager for "srl_quad" raptor
   FlightManager fm_1(std::make_unique<Raptor>("selva1"));
 
+  // add go over object behaviour to fm_1 flight manager
   fm_1.add_behaviour(GoOverObject::get());
 
   fm_1.behav_register.at(0)->start();
